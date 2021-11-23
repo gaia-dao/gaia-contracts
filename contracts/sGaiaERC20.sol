4a48368a684fc72da8084727eb59c57b1858b8fa
nateliason <nateliason@gmail.com>
2021-11-23 14:37:55 -0600

    discord test 2

--- contracts/sGaiaERC20.sol
+++ contracts/sGaiaERC20.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 // Notes on this contract here: https://app.clarity.so/gaia1/notes/38012ab6-bf5e-40c8-93ce-3dc571d07121
-
 pragma solidity 0.7.5;
 
 
